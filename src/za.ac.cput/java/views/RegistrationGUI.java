4de5b2fc352c9e7874fbf5dd54bc35d1d10a5b2a
Marvinhope24 <80388453+Marvinhope24@users.noreply.github.com>
2022-10-22 15:58:09 +0200

    Add files via upload

---
+++ src/za.ac.cput/java/views/RegistrationGUI.java
@@ -0,0 +1,12 @@
+package views;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegistrationGUI extends JFrame implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
